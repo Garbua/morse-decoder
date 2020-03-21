9e7a571bfe5e2cb6f79f948ba8ef13fb9293b37d
Garbua <denisgarbuza@yandex.ru>
2020-03-21 21:49:49 +0300

    feat: add solution morse-code-decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function converting(str) {
+    let arrAbc = [];
+    for(let i = 0; i < str.length; i += 10) {
+        let abc = str.slice( i, (i + 10) );
+        arrAbc.push(abc);
+    }
+      return  arrAbc; 
+}
+
+
 function decode(expr) {
-    // write your solution here
+    let abc = converting(expr).map((el) => {
+        let symbol = el.replace(/00/gi, '');
+        el = symbol;
+        symbol = el.replace(/10/gi, '.');
+        el = symbol;
+        symbol = el.replace(/11/gi, '-');
+        el = symbol;
+        return el;
+    })
+
+    let letters = abc.map((item) => {
+        if(item in MORSE_TABLE) {
+            item = MORSE_TABLE[item];
+        }
+        if(item === '**********') {
+            item = ' ';
+        }
+        return item;
+    })
+
+   let result = letters.join('');
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
